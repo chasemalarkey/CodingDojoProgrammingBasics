36010f3e0586d3fedb4b1b7f19a027763baf5d66
Chase Malarkey <127543574+chasemalarkey@users.noreply.github.com>
2023-04-19 19:57:13 -0400

    Add files via upload
    Working with parameters!

---
+++ Malarkey_Chase_JS_Parameters.js
@@ -0,0 +1,23 @@
+// function greeting(name){
+//     console.log(`Good day, ${name}`)
+// }
+// greeting("Anakin");
+
+// Level 2
+
+// function greeting(name,time){
+//     console.log(`Good day, ${name}`)
+//     console.log(`The time is ${time}`)
+// }
+// greeting("Anakin", "5:46 P.M.");
+
+// Level 3
+
+function greeting(name){
+    if(name == "Count Dooku"){
+        console.log("Im coming for you, " + name)
+    } else {
+    console.log(`Good day, ${name}`)
+    }
+}
+greeting("Count Dooku");
